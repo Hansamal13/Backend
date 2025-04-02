dce98435ff528cf8df4f6d8e0d0978c0d8d3e2f0
Sachin Sasanka <sasankahansamal213@gmail.com>
2025-04-02 20:49:08 +0530

    Add files via upload

---
+++ Model/ArtistControler.js
@@ -0,0 +1,29 @@
+const Artist = require("../Model/UserControlers");
+
+const getAllArtists = async(req, res , next) =>{
+
+    let Artists;
+
+   
+   //All users
+    try{
+        artists = await Artist.find();
+    }catch(err){
+        console.log(err);
+    }
+
+    if(!artists){
+        return res.status(404).json({Message:"Not found"});
+    }
+
+
+    //dispplay
+
+    return res.status(200).json({artists});
+
+
+
+};
+
+
+exports.getAllArtists = this.getAllArtists;
