d89fc28d94974138629f2eed7dc32ac7e4d0b97f
Sachin Sasanka <sasankahansamal213@gmail.com>
2025-04-02 20:49:38 +0530

    Add files via upload

---
+++ Route/ArtistRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const artist = express.Router();
+
+//Insert model
+const Artist = require("../Model/ArtistModel");
+
+//artist controler
+const ArtistControler = require("../Controlers/ArtistControler");
+
+router.get("/",ArtistControler.getAllArtist);
+
+router.post("/", ArtistControler.addArtists);
+router.get("/:id", ArtistControler.getById);
+router.put("/:id", ArtistControler.updateArtists);
+router.delete("/:id", ArtistControler.deleteArtists);
+
+//export
+module.exports = artist;
