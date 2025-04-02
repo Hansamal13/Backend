2a068adcc2dd7205ed3c209f0f65672ce41d3a97
Sachin Sasanka <sasankahansamal213@gmail.com>
2025-04-02 20:50:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import artRoutes from './Route/artRoute.js';
+// password 95xjXJ85og58yh7H
 
-// Load environment variables
-dotenv.config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./Route/UserRoutes");
+
+
 
 const app = express();
+const cors = require("cors");
 
-// Middleware
-app.use(cors());
+
+//middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/users",router);
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
 
-// Routes
-app.use('/art', artRoutes);
+mongoose.connect("mongodb+srv://sachin13:95xjXJ85og58yh7H@cluster0.2qdiv.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('Art Gallery API is running');
-});
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: 'Something went wrong',
-    error: process.env.NODE_ENV === 'development' ? err.message : null
-  });
-});
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-export default app;
