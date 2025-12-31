d91fbf10cc0d2582b12c448b059d21be888029a1
Sachin Sasanka <sasankahansamal213@gmail.com>
2025-12-31 21:26:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 // password 95xjXJ85og58yh7H
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./Route/UserRoutes");
 
-
-
 const app = express();
 const cors = require("cors");
-
 
 //middleware
 app.use(express.json());
 app.use(cors());
 app.use("/users",router);
-
 
 mongoose.connect("mongodb+srv://sachin13:95xjXJ85og58yh7H@cluster0.2qdiv.mongodb.net/")
 .then(()=> console.log("Connected to MongoDB"))
@@ -23,6 +17,7 @@ mongoose.connect("mongodb+srv://sachin13:95xjXJ85og58yh7H@cluster0.2qdiv.mongodb
     app.listen(5000);
 })
 .catch((err)=> console.log((err)));
+
 
 
 
